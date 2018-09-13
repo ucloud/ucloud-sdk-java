0b2ed66c2c51fbc9b82a03580ed69768bd84cb1e
codezhang <zjhfyq@vip.qq.com>
2018-09-13 10:50:21 +0800

    check dev

---
+++ ucloud-sdk-java-uhost/src/main/java/cn/ucloud/model/GetUHostInstanceVncInfoParam.java
@@ -0,0 +1,12 @@
+package cn.ucloud.model;
+
+/**
+ * @description: 获取指定UHost实例的管理VNC配置详细信息
+ * @author: codezhang
+ * @date: 2018-09-13 10:44
+ **/
+
+public class GetUHostInstanceVncInfoParam {
+
+
+}
