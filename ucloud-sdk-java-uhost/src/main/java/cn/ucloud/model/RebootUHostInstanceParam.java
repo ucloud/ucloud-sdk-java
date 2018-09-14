e8e1f9a780930a7576293e1e2131673079084131
codezhang <zjhfyq@vip.qq.com>
2018-09-14 18:35:55 +0800

    完成RebootUHostInstance,修改BaseRequestParam(增加projectId),修改ParamConstructor（调换参数校验的顺序）

---
+++ ucloud-sdk-java-uhost/src/main/java/cn/ucloud/model/RebootUHostInstanceParam.java
@@ -0,0 +1,10 @@
+package cn.ucloud.model;
+
+/**
+ * @description:
+ * @author: codezhang
+ * @date: 2018-09-14 17:52
+ **/
+
+public class RebootUHostInstanceParam {
+}
---
+++ ucloud-sdk-java-uhost/src/test/java/cn/ucloud/client/RebootUhostInstanceTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * @description:
+ * @author: codezhang
+ * @date: 2018-09-14 18:17
+ **/
+public class RebootUhostInstanceTest {
+
+}
