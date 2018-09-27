f2cea51df5cc4eec768e2187a5622d1edca7e316
codezhang <zjhfyq@vip.qq.com>
2018-09-27 17:32:37 +0800

    完成DescribeShareBandwidth

---
+++ ucloud-sdk-java-unet/src/main/java/cn/ucloud/model/DescribeShareBandwidthParam.java
@@ -0,0 +1,10 @@
+package cn.ucloud.model;
+
+/**
+ * @description:
+ * @author: codezhang
+ * @date: 2018-09-27 16:52
+ **/
+
+public class DescribeShareBandwidthParam {
+}
---
+++ ucloud-sdk-java-unet/src/test/java/cn/ucloud/client/DescribeShareBandwidthTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * @description:
+ * @author: codezhang
+ * @date: 2018-09-27 17:16
+ **/
+public class DescribeShareBandwidthTest {
+
+}
