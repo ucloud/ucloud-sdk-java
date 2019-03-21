ae9b34dae4e708c70f568b558f6db0022d06c44b
codezhang <jianhui.zhang@ucloud.cn>
2019-03-21 23:38:35 +0800

    change ignore

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git filter-branch --force --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch ucloud-sdk-generator-1.0-SNAPSHOT.jar' --tag-name-filter cat --
+git filter-branch --force --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch sonar_analyze.sh' --tag-name-filter cat --
+git filter-branch --force --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch template' --tag-name-filter cat --
