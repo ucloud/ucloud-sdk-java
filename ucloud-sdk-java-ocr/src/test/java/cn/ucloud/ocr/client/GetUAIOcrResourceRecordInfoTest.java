ae711082b199a1fa506e6c7bf8638bbcc7533280
codezhang <jianhui.zhang@ucloud.cn>
2019-04-19 16:43:55 +0800

    complete ocr api

--- ucloud-sdk-java-ocr/src/test/java/cn/ucloud/ocr/client/GetUAIOcrResourceRecordInfoTest.java
+++ ucloud-sdk-java-ocr/src/test/java/cn/ucloud/ocr/client/GetUAIOcrResourceRecordInfoTest.java
@@ -1,24 +1,25 @@
 package cn.ucloud.ocr.client;
 
 import cn.ucloud.common.pojo.Account;
-import cn.ucloud.ocr.model.ModifyUAIOcrResourceNameParam;
-import cn.ucloud.ocr.model.ModifyUAIOcrResourceNameResult;
+import cn.ucloud.ocr.model.GetUAIOcrResourceRecordInfoParam;
+import cn.ucloud.ocr.model.GetUAIOcrResourceRecordInfoResult;
+import cn.ucloud.ocr.model.ModifyUAIOcrResourceMemoParam;
+import cn.ucloud.ocr.model.ModifyUAIOcrResourceMemoResult;
 import cn.ucloud.ocr.pojo.OcrConfig;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 /**
  * @Description :
  * @Author : codezhang
- * @Date : 2019-04-19 16:04
+ * @Date : 2019-04-19 16:36
  **/
-public class ModifyUAIOcrResourceNameTest {
-
+public class GetUAIOcrResourceRecordInfoTest {
     private OcrClient client;
 
-    private ModifyUAIOcrResourceNameParam param;
+    private GetUAIOcrResourceRecordInfoParam param;
 
     @Before
     public void setUp() throws Exception {
@@ -27,17 +28,17 @@ public class ModifyUAIOcrResourceNameTest {
                         System.getenv("UCloudPublicKey"))));
         String projectId = System.getenv("ProjectId");
         String region = "cn-bj2";
-        String resourceId = "uaiocr-uw4cd44y";
-        String resourceName = "sdk-test-name";
-        param = new ModifyUAIOcrResourceNameParam(region,resourceId,resourceName);
+        Long beginTime = 1555557017L;
+        Long endTime = 1555638570L;
+        param = new GetUAIOcrResourceRecordInfoParam(region,beginTime,endTime);
         param.setProjectId(projectId);
     }
 
     @Test
-    public void modifyUAIOcrResourceName() {
+    public void getUAIOcrResourceRecordInfo() {
         try {
-            ModifyUAIOcrResourceNameResult result =
-                    client.modifyUAIOcrResourceName(param);
+            GetUAIOcrResourceRecordInfoResult result =
+                    client.getUAIOcrResourceRecordInfo(param);
             JSONComparator.jsonComparator(result);
         } catch (Exception e) {
             assertNull(e);
