2a9ddf1ee6c69c3e08c9c063dee590448890965f
codezhang <jianhui.zhang@ucloud.cn>
2019-04-23 16:58:32 +0800

    uhost.CreateUhostInstance支持批量

--- ucloud-sdk-java-censor/src/test/java/cn/ucloud/censor/client/GetUAICensorResourceMetricInfoTest.java
+++ ucloud-sdk-java-censor/src/test/java/cn/ucloud/censor/client/GetUAICensorResourceMetricInfoTest.java
@@ -22,13 +22,11 @@ public class GetUAICensorResourceMetricInfoTest {
 
     @Before
     public void setUp() throws Exception {
-        // todo 测试
         client = new DefaultCensorClient(new CensorConfig(
                 new Account(System.getenv("UCloudPrivateKey"),
                         System.getenv("UCloudPublicKey"))));
         String projectId = System.getenv("ProjectId");
         String region = "cn-bj2";
-        String zone = "cn-bj2-04";
         Long beginTime = 1555557017L;
         Long endTime = 1555578617L;
         String resourceId = "uaicensor-nkh1osfy";
