ee6ddf2d475967779bcb198ae6476e761d55e5f2
codezhang <jianhui.zhang@ucloud.cn>
2019-06-04 17:45:11 +0800

    增加USMS.SendUSMSMessage

---
+++ ucloud-sdk-java-usms/src/main/java/cn/ucloud/usms/client/USMSClient.java
@@ -0,0 +1,36 @@
+package cn.ucloud.usms.client;
+
+import cn.ucloud.common.client.UcloudClient;
+import cn.ucloud.common.handler.UcloudHandler;
+import cn.ucloud.usms.model.SendUSMSMessageParam;
+import cn.ucloud.usms.model.SendUSMSMessageResult;
+
+/**
+ * @Description : USMS 客户端接口
+ * @Author : codezhang
+ * @Date : 2019年06月04日
+ **/
+public interface USMSClient extends UcloudClient {
+
+
+    /**
+     * 发送短信息
+     *
+     * @param param 请求参数对象
+     * @return 结果对象
+     * @throws Exception 请求出错则抛出异常
+     */
+    SendUSMSMessageResult sendUSMSMessage(SendUSMSMessageParam param) throws Exception;
+
+    /**
+     * 发送短信息
+     *
+     * @param param     请求参数对象
+     * @param handler   异步处理器
+     * @param asyncFlag 异步标记位，默认true异步
+     */
+    void sendUSMSMessage(SendUSMSMessageParam param, UcloudHandler<SendUSMSMessageResult> handler,
+                     Boolean... asyncFlag);
+
+
+}
