c56eb7ade74ceca32d5e8fb8724675477d8e74dd
peggypig <jianhui.zhang@ucloud.cn>
2019-08-01 10:10:31 +0800

    UMem.UMemDataSet增加字段

--- ucloud-sdk-java-umem/src/main/java/cn/ucloud/umem/model/UMemDataSet.java
+++ ucloud-sdk-java-umem/src/main/java/cn/ucloud/umem/model/UMemDataSet.java
@@ -153,6 +153,17 @@ public class UMemDataSet {
     @SerializedName("VPCId")
     private String vpcId;
 
+    @SerializedName("RewriteTime")
+    private Integer rewriteTime;
+
+    public Integer getRewriteTime() {
+        return rewriteTime;
+    }
+
+    public void setRewriteTime(Integer rewriteTime) {
+        this.rewriteTime = rewriteTime;
+    }
+
     public String getOwnSlave() {
         return ownSlave;
     }
