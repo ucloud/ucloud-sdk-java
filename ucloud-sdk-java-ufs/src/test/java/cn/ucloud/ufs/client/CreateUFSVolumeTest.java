f522dc408c26497361208f6f3b264b88f97f4c6e
peggypig <jianhui.zhang@ucloud.cn>
2019-08-28 13:45:35 +0800

    UFS.CreateUFSVolumeTest修改测试

--- ucloud-sdk-java-ufs/src/test/java/cn/ucloud/ufs/client/CreateUFSVolumeTest.java
+++ ucloud-sdk-java-ufs/src/test/java/cn/ucloud/ufs/client/CreateUFSVolumeTest.java
@@ -25,7 +25,7 @@ public class CreateUFSVolumeTest {
         client = new DefaultUFSClient(new UFSConfig(
                 new Account(System.getenv("UCloudPrivateKey"),
                         System.getenv("UCloudPublicKey"))));
-        param = new CreateUFSVolumeParam("cn-bj2",1024);
+        param = new CreateUFSVolumeParam("cn-bj2",1024,"Basic");
         param.setProjectId("org-izug1m");
     }
     @Test
