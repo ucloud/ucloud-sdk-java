3e6b4967b924329afa3aba69510511944a88bb0d
peggypig <jianhui.zhang@ucloud.cn>
2019-10-31 13:38:41 +0800

    update ignore

--- sonar_analyze.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mvn compile sonar:sonar -Dsonar.host.url=$SONAR_HOST -Dsonar.login=$SONAR_TOKEN -Dsonar.issuesReport.html.enable=true -Dsonar.projectKey=$CI_PROJECT_PATH_SLUG -Dsonar.gitlab.project_id=$CI_PROJECT_ID -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA -Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME
-
-if [ $? -eq 0 ]; then
-    echo "sonarqube code-analyze over."
-fi
